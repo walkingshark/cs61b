317929c43860975c26a3c405819d0e3aba7346c5
walkingshark <s10131416@gamil.com>
2024-08-30 12:24:56 +0800

    atLeastOneMoveExists: finished(compare value instead of tile itself)

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -176,9 +176,10 @@ public class Model extends Observable {
         } else{
             for (int i = 0; i < b.size(); i++){
                 for (int j = 0; j < b.size(); j++){
-                    if (i != 0 && b.tile(i-1, j) ==  b.tile(i, j)){
+                    if (i != 0 && b.tile(i-1, j).value() ==  b.tile(i, j).value()){
                         ;return true;
-                    } else if (j != 0 && b.tile(i, j-1) == b.tile(i, j)) {
+                    }
+                    if (j != 0 && b.tile(i, j-1).value() == b.tile(i, j).value()) {
                         return true;
                     }
                 }
