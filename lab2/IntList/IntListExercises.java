cc32255d3689e3611491c6f4ead092a73e19e95d
walkingshark <s10131416@gamil.com>
2024-09-04 14:50:16 +0800

    try to fix empty intlist

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,7 +14,9 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
-        head.first += c;
+        if (head != null){
+            head.first += c;
+        }
     }
 
     /**
