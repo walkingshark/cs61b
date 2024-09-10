d4cfb39df16d519b4b911308f30e85833fadc067
walkingshark <s10131416@gamil.com>
2024-09-10 19:45:07 +0800

    move AList to ArrayDeque

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,4 +1,61 @@
 package deque;
+public class ArrayDeque<Item> {
+    private int size;
+    private Item[] items;
+    /** Creates an empty list. */
+    public ArrayDeque() {
+        items = (Item[]) new Object[100];
+        size = 0;
+    }
+    // resizing the array
+    public void resize(int capacity) {
+        Item[] temp = (Item[]) new Object[capacity];
+        System.arraycopy(items, 0, temp, 0, size);
+        items = temp;
+    }
+    /** Inserts X into the back of the list. */
+    public void addLast(Item x) {
 
-public class ArrayDeque {
+        if (size == items.length){
+            resize(size * 2);
+        }
+        items[size] = x;
+        size++;
+    }
+
+    /** Returns the item from the back of the list.*/
+    public Item getLast() {
+        return items[size-1];
+    }
+    /** Gets the ith item in the list (0 is the front). */
+    public Item get(int i) {
+        return items[i];
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Deletes item from back of the list and
+     * returns deleted item. */
+    public Item removeLast() {
+        Item result = getLast();
+        items[size - 1] = null;
+        size--;
+        return result;
+    }
+    public void addFirst(Item item){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public void printDeque(){
+
+    }
+    public Item removeFirst(){
+
+    }
+
 }
