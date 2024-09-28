a1801b76026ee16f6942490a846a62f6943cc491
walkingshark <s10131416@gamil.com>
2024-09-28 13:24:35 +0800

    get broken collatz back

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,14 @@
  */
 public class Collatz {
 
-    /** if n is odd, return 3n+1, even: n/2,
-     * if n is 1: return 1*/
-    public static int nextNumber(int n){
-        if (n == 1) {
+    /** Buggy implementation of nextNumber! */
+    public static int nextNumber(int n) {
+        if (n  == 128) {
             return 1;
-        } else if (n % 2 == 1) {
+        } else if (n == 5) {
             return 3 * n + 1;
         } else {
-            return n / 2;
+            return n * 2;
         }
     }
 
