113fa5a7f30746fab3e772ecc6c173dbe8183ac8
walkingshark <s10131416@gamil.com>
2024-10-21 22:22:40 +0800

    add blob

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -1,4 +1,21 @@
 package gitlet;
 
+import java.io.File;
+
+import static gitlet.Utils.*;
+
 public class Blob {
+    public static final File CWD = new File(System.getProperty("user.dir"));
+    /** The .gitlet directory. */
+    public static final File GITLET_DIR = join(CWD, ".gitlet");
+    public File blobs = join(GITLET_DIR, "blobs");
+    public File content;
+    public String id;
+    public Blob() {
+        // read and write
+        String s = readContentsAsString(CWD);
+        id = sha1(s);
+        content = join(blobs, id);
+        writeContents(content, s);
+    }
 }
